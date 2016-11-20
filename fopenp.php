b5dda224a5c2a73dae0e5c4f3b857d92d2ef8efe
Dan Cobb <cobbdb@gmail.com>
2016-11-19 18:32:38 -0800

    .

---
+++ fopenp.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Create or open file at given path.
+ * @param {string} path
+ * @return {resource}
+ */
+function fopenp($path) {
+    $dirname = dirname($path);
+    if (!is_dir($dirname)) {
+        mkdir($dirname, 0755, true);
+    }
+    return fopen($path, 'w');
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# PHP fopen+
+
+```php
+include_once("$MODULES/fopen-plus/fopenp.php");
+
+$fout = fopenp('path/to/some.file');
+```
